bf4020cd33cd32924e56acfcf74cdc8a17a812c6
auropreioni <aurora.preioni@mail.polimi.it>
2025-10-18 14:10:30 +0200

    petali

--- sketch.js
+++ sketch.js
@@ -15,6 +15,8 @@ function preload() {
 
 function setup() {
 
+  angleMode(DEGREES); //dico che voglio usare i gradi e non i radianti 
+
  // copio qui i paramteri per poterli usare anche in setup per calcolare la dimensione del canvas
   let lato = 60;
   let paddingEsterno = 20;
@@ -41,10 +43,15 @@ function draw() {
 
 function disegnaGriglia(){
 
-  let valori1 = table.getColumn(1).map(Number);
+  //variabili colonna 1
+  let valori1 = table.getColumn(1).map(Number); //ARRAY 1
   let min1 = min(valori1);
   let max1 = max(valori1)
 
+  //variabili colonna 3
+  let valori3 = table.getColumn(3).map(Number); //ARRAY 3
+  let min3 = min(valori3);
+  let max3 = max(valori3);
 
   background(255); //metto uno sfondo 
 
@@ -113,7 +120,14 @@ function disegnaGriglia(){
     fill(colore);        // metto qui la mia nuova variabile che determina il colore dei quadrati 
     rect(x, y, lato, lato, 7);
    
+    //metto prima i petali così stanno sotto
+    disegnaPetali(riga, x, y, lato, min3, max3);
+    //riga: dove prendo di dati 
+    //x è cordinata dell'angolo in alto a sinistra del quadrato, y uguale a sopra 
+    //lato è il lato del quadrato 
+    //min3 valore minimo e max3 valore massimo (sempre della colonna3)
     disegnaCentro(riga, x, y, lato, min1, max1);
+
   }
 
 }
@@ -121,7 +135,8 @@ function disegnaGriglia(){
 function disegnaCentro (riga, x, y, lato, min1, max1){
   let v1 = riga.getNum(1); //legge il valore della colonna 1 associato alla riga
 
-  let valori2 = table.getColumn(2).map(Number);
+  //specifico variabili colonna 2
+  let valori2 = table.getColumn(2).map(Number); //ARRAY 2
   let min2 = min(valori2);
   let max2 = max(valori2)
 
@@ -161,6 +176,72 @@ function disegnaCentro (riga, x, y, lato, min1, max1){
   circle(0, 0, lato);
   pop ();
 }
+
+}
+
+function disegnaPetali(riga, x, y, lato, min3, max3) {
+
+  let v3 = riga.getNum(3); //leggo i valori associati alle righe della colonna 3 
+
+let angolo; //creo una nuova variabile 
+  if (isNaN(v3) || min3 === max3) { //isNaN v mi dice di controllare che v sia un numero valido 
+  angolo = 0; // caso particolare, se tutti i valori sono uguali, angolo = 0
+  } else {
+  angolo= map(v3, min3, max3, 0, 360); 
+  //uso map per associare le due scale
+
+  //specifico variabili colonna 4
+  let valori4 = table.getColumn(4).map(Number); //ARRAY 4
+  let min4 = min(valori4);
+  let max4 = max(valori4)
+
+  let coloreMin4 = color(255,228,225);
+  let coloreMax4 = color(204,51,102);
+
+
+  //DISEGNO I PETALI 
+  //definisco qualche variabile 
+  let nPetali = 8;
+  let step = 360/nPetali; 
+  let distPetali = lato*0.28;
+  let wPetali = lato*0.32;
+  let hPetali = lato*0.16;
+
+  let v4 = riga.getNum(4); //legge il numero della prima colonna associato a quella riga 
+
+    let f; //creo una nuova variabile 
+  if (isNaN(v4) || min4 === max4) { //isNaN v mi dice di controllare che v sia un numero valido 
+  f = 0.5; // caso particolare, se tutti i valori sono uguali, t = 0.5
+  } else {
+  f = map(v4, min4, max4, 0, 1);
+  //uso map per associare il vMin a 0 e il vMax a 1, map lavora solo con numeri e non con i colori 
+  }
+
+  let colorePetali = lerpColor(coloreMin4, coloreMax4, f);
+//mischia i colori 
+
+  push(); //creo la mia bolla 
+  //traslo il punto di applicazione nel centro del quadrato 
+  translate(x + lato / 2, y + lato / 2);
+ 
+  //colore del petalo 
+  noStroke();
+  fill(colorePetali);
+
+   // Disegno 8 petali come ellissi ruotate
+  for (let k = 0; k < nPetali; k++) { //k è il mio petalo
+    push(); //serve per applicare la trasformazione solo qui 
+    rotate(angolo + k * step);
+    ellipse(distPetali, 0, wPetali, hPetali); 
+    //distPetali è il centro del fiore 
+    pop();
+  }
+
+  pop();
+  }
+
+
+
 
 }
 
